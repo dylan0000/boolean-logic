e2f01b8e9638296ef330f934d3b55e05fe6807f5
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-02-06 13:13:38 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+// 1. if two things are true, what is the result
+// NOTE: Use The AND operator. Write code here
+
+if (true && true)
+
+    game.splash("true and true is true")
+
+// 2. if one thing is true and one thing is false what is the result
+// NOTE: Use The AND operator. Write code here
+
+if (true && false)
+
+    game.splash("true and false is false")
+
+// 3. if two things are false, what is the result
+// NOTE: Use The AND operator. Write code here
+
+if (false && false)
+
+    game.splash("false and false is false")
+
+// 4. if two things are true, what is the result
+// NOTE: Use The OR operator. Write code here
+
+if (true || true)
+
+    game.splash("true or true is true")
+
+// 5. if one thing is true and one thing is false what is the result
+// NOTE: Use The OR operator. Write code here
+
+if (true || false)
+
+    game.splash("true or false is true")
+
+// 6. if two things are false, what is the result
+// NOTE: Use The OR operator. Write code here
+
+if (false || false)
+
+    game.splash("false or false is false")
+    game.reset()
